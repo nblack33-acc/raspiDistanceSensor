3d9f0f2b06676dda9118ebd86a7a560e4ac6fa72
nblack33-acc <97971711+nblack33-acc@users.noreply.github.com>
2022-04-14 11:43:53 -0400

    Added first attempt at distance sensor script
    Is it really this easy? I'm paranoid

---
+++ distance.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from gpiozero import DistanceSensor
+from time import sleep
+
+led = LED(25)
+sensor = DistanceSensor(echo=24, trigger=23, max_distance=2.0)
+while True:
+	distance = sensor.distance * 100
+	print("Distance : %.1f" % distance)
+	if distance < 1000:
+		led.on()
+	else:
+		led.off()
+	sleep(1)
